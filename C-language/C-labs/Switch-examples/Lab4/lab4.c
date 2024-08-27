71577ad1c2edfc75c895fc7f3d83841505d7e397
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-08-27 21:00:53 +0300

    calculate the circle area and circumference use Switch statement

---
+++ C-language/C-labs/Switch-examples/Lab4/lab4.c
@@ -0,0 +1,48 @@
+/*
+ * lab4.c
+ *
+ *  Created on: Aug 27, 2024
+ *      Author: Sabri
+ *  first Lab in C programing languge
+ *  this code aim to calculate the circle area and circumference.
+ */
+
+# include <stdio.h>
+
+int main()
+{
+	char choise;
+	float radios, Area, Circumf, Pi=3.14;
+	printf("Enter the radios = ");
+	fflush(stdout);
+	scanf("%f",&radios);
+	printf("\nEnter A for Area or C for Circumference = ");
+	fflush(stdout);
+	scanf("\n %c",&choise);
+
+	switch(choise)
+	{
+	case('a'):
+	case('A'):
+	{
+		Area=radios*radios*Pi;
+		printf("\nArea = %f",Area);
+	}
+	break;
+	case('c'):
+	case('C'):
+	{
+		Circumf=2*radios*Pi;
+		printf("\nCircumference = %f",Circumf);
+	}
+	break;
+	default:
+	{
+		printf("\nTry again");
+	}
+	break;
+	}
+
+	return 0;
+}
+
