b413783697fc1f5fd3d630b47c01bb0a926226ff
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-08-30 18:33:09 +0300

    example1

---
+++ C-language/C-assignments/01_C-Basic/01- homework01/01/01.c
@@ -0,0 +1,20 @@
+/*
+EX1:
+
+Write C Program to Print a Sentence
+
+i should see the Console as following:
+##########Console-output###
+C Programming
+###########################
+
+ */
+
+# include <stdio.h>
+
+int main()
+{
+   printf("##########Console-output###");
+   printf("\nC Programming");
+   printf("\n###########################");
+}
