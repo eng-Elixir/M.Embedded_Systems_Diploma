dcd7893bcb1a72e717351341bdd1ab123cef9051
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-08-31 11:16:00 +0300

    homework 2 examples

---
+++ C-language/C-assignments/01_C-Basic/01- homework01/05/05.c
@@ -0,0 +1,22 @@
+/*
+EX5:
+
+Write C Program to Find ASCII Value of a Character
+
+#########Console_output######
+Enter a character: G
+ASCII value of G = 71
+#############################
+*/
+
+# include <stdio.h>
+
+int main()
+{
+   char Ch;
+   printf("Enter a character:");
+   fflush(stdout);
+   scanf("\t%c",&Ch);
+   printf("\nASCII value %d",Ch);
+   
+}
