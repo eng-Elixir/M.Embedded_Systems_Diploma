9f8003f06802f70567a8d816ce12189385b3d257
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-09-07 15:29:47 +0300

    Array example lab1

---
+++ C-language/C-labs/08- Arrays/lab1/lab1.c
@@ -0,0 +1,21 @@
+/*
+ * Main.c
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: Sabri
+ *      Array Example
+ */
+# include <stdio.h>
+
+int main()
+
+{
+	float degrees[100];
+	float x=9.9,y;
+	degrees[4]=x;
+	y = degrees[4];
+	scanf("%f", &degrees[7]);
+	printf("the eight item value = %0.2f", degrees[7]);
+
+}
+
