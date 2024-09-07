3dfe34068586a0444c2811e4397293ad7bbb9b9b
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-09-07 20:40:37 +0300

    array lab7 example

---
+++ C-language/C-labs/08- Arrays/Lab7/lab7.c
@@ -0,0 +1,18 @@
+/*
+ * Main.c
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: Sabri
+ *      Array Example
+ */
+# include <stdio.h>
+
+int main()
+
+{
+	char text[100];
+	printf("\nenter your name: ");
+	gets(text);
+	printf("\nyour name is : %s",text);
+}
+
