0d0eec0bd0a7f6b931a54d2429867efbf9a9f73a
Sabri Abdulhq <sabriabdulhq@gmail.com>
2024-09-07 20:41:32 +0300

    Array Example

---
+++ C-language/C-labs/08- Arrays/Lab6/lab6.c
@@ -0,0 +1,17 @@
+/*
+ * Main.c
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: Sabri
+ *      Array Example
+ */
+# include <stdio.h>
+
+int main()
+
+{
+    char text[100]={'h','e','l','l','o',0};
+    printf("%s \n", text);
+
+}
+
